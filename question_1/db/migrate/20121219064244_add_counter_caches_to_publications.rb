7e0bd4261f4f610c8efff54f2855fd936b20de04
Akira Matsuda <ronnie@dio.jp>
2012-12-19 16:02:07 +0900

    [A1] enable counter_caches && set default counter_cache values

--- question_1/db/migrate/20121219064244_add_counter_caches_to_publications.rb
+++ question_1/db/migrate/20121219064244_add_counter_caches_to_publications.rb
@@ -1,6 +1,10 @@
 class AddCounterCachesToPublications < ActiveRecord::Migration
   def change
-    add_column :publications, :concerns_count, :integer
-    add_column :publications, :publication_comments_count, :integer
+    add_column :publications, :concerns_count, :integer, default: 0
+    add_column :publications, :publication_comments_count, :integer, default: 0
+
+    Publication.find_each do |pub|
+      Publication.reset_counters pub.id, :concerns, :publication_comments
+    end
   end
 end
