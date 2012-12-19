52e7aa8546f0410c3cacdff8540c5a1da4b2d9c6
Akira Matsuda <ronnie@dio.jp>
2012-12-19 16:37:25 +0900

    [A2] use transaction

--- question_2/app/controllers/publications_controller.rb
+++ question_2/app/controllers/publications_controller.rb
@@ -13,14 +13,15 @@ class PublicationsController < ApplicationController
   end
 
   def create
-    @publication = Publication.new params[:publication]
-    @publication.users << current_user
-
-    if @publication.save
-      redirect_to @publication, notice: "#{@publication.title}を登録しました"
-    else
-      render 'new'
+    begin
+      ActiveRecord::Base.transaction do
+        @publication = Publication.create! params[:publication]
+        @publication.users << current_user
+      end
+    rescue ActiveRecord::RecordInvalid
+      return render 'new'
     end
+    redirect_to @publication, notice: "#{@publication.title}を登録しました"
   end
 
   def add_author
