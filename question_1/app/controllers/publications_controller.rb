87f27f7f27b6673744fd147a851ce7e2cf10e7ef
Akira Matsuda <ronnie@dio.jp>
2012-12-19 15:32:17 +0900

    [A1] use includes + length to avoid N+1 queries
    * eager load concerns and publication_comments using Relation#includes
    * replace Relation#count, which invokes select count SQL, with Relation#length which will be delegated to Array#length

--- question_1/app/controllers/publications_controller.rb
+++ question_1/app/controllers/publications_controller.rb
@@ -1,6 +1,6 @@
 # coding: utf-8
 class PublicationsController < ApplicationController
   def index
-    @publications = Publication.all
+    @publications = Publication.includes(:concerns, :publication_comments)
   end
 end
