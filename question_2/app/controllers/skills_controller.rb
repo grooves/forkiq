321417cf47a4eea119114952d911fa5f1516a234
Akira Matsuda <ronnie@dio.jp>
2012-12-19 17:17:22 +0900

    [A2] treat irregular cases as system errors

--- question_2/app/controllers/skills_controller.rb
+++ question_2/app/controllers/skills_controller.rb
@@ -3,11 +3,7 @@ class SkillsController < ApplicationController
   def create
     @publication = Publication.find params[:publication_id]
     @skilltag = Skilltag.where(name: params[:skilltag]).first_or_create!
-
-    if @publication.skilltags << @skilltag
-      redirect_to @publication, notice: "#{@publication.title}のスキルに#{@skilltag.name}を追加しました"
-    else
-      redirect_to @publication, notice: "#{@publication.title}のスキルに#{@skilltag.name}を追加できませんでした"
-    end
+    @publication.skilltags << @skilltag
+    redirect_to @publication, notice: "#{@publication.title}のスキルに#{@skilltag.name}を追加しました"
   end
 end
